3b27b833051a9076d894155f2688df86a581d3d1
Negar Amiri <negamiri@gmail.com>
2019-01-09 00:43:16 +0000

    Add chatbar and messages components

---
+++ src/ChatBar.jsx
@@ -0,0 +1,16 @@
+require("../styles/home.scss");
+import React, {Component} from 'react';
+
+
+export default class ChatBar extends Component {
+    render() {
+        return (
+            <div>
+                <footer className="chatbar">
+                    <input className="chatbar-username" placeholder="Your Name (Optional)" />
+                    <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+                </footer>
+            </div>
+        )
+    }
+}
---
+++ src/Messages.jsx
@@ -0,0 +1,20 @@
+require("../styles/home.scss");
+import React, {Component} from 'react';
+
+
+export default class Messages extends Component {
+    render() {
+        return(  
+        <div>
+            <main className="messages">
+                <div className="message">
+                <span className="message-username">Anonymous1</span>
+                <span className="message-content">I won't be impressed with technology until I can download food.</span>
+                </div>
+                <div className="message system">
+                Anonymous1 changed their name to nomnom.
+                </div>
+            </main>
+      </div>)
+    }
+}
